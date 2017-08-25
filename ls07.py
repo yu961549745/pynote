5976aeeca6368c0956baddf6a9ccb93ae8e0612a
yjt <961549745@qq.com>
2017-08-26 00:15:25 +0800

    尝试Python继承

--- ls07.py
+++ ls07.py
@@ -1,3 +1,30 @@
 '''
 面向对象编程
 '''
+
+
+class A(object):
+    def __init__(self, x):
+        self.A = x
+
+    def hello(self):
+        pass
+
+
+class B(A):
+    def __init__(self, x, y):
+        super().__init__(x)
+        self.B = y
+
+    def __str__(self):
+        return 'A=%d,B=%d' % (self.A, self.B)
+
+    def hello(self):
+        print("hello")
+
+
+a = A(1)
+b = B(1, 2)
+print(b)
+b.hello()
+a.hello()
